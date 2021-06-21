3096e24624fa57406b1b9755d86428394eac267e
aziz-sassi <sassiaziz50@gmail.com>
2021-06-21 14:35:16 +0100

    creating backend and adding basic routes

---
+++ project/server/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+
+// const router = require("./routes/lanza.routes");
+
+
+
+
+const app = express();
+
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to lanza-libre application." });
+});
+
+
+// set port, listen for requests
+require("./routes/lanza.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+// app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
